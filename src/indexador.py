74d62b22767f38232eb52b9d2d343e32bd4edb32
NicolasBenjaminCaceresMontenegro <88669210+NicolasBenjaminCaceresMontenegro@users.noreply.github.com>
2021-10-14 19:09:41 -0300

    ordena los valores
    mantiene las key ordenadas
    falta:
    por ahi se pueda sacar un metodo para los pares : id usuario, id tweet, id fecha, por ahi otro diferente para los pares frase id, palabra id
    dejarlo en disco? manterlo en memoria?
    debug

--- src/indexador.py
+++ src/indexador.py
@@ -15,7 +15,6 @@ class Indexador():
 
     def armar_indices(self):
         nombre_archivo = os.path.abspath("fetched_tweets.csv")
-        print(nombre_archivo)
         with open(nombre_archivo, "r", encoding="utf-8") as lector_csv:
             lector = csv.DictReader(lector_csv, delimiter=",")
             indice_id_tweet = {}
@@ -28,7 +27,7 @@ class Indexador():
             pares_id_usuario = []
             pares_frase_id = []
             pares_fecha_id = []
-            
+
             for linea in lector:
                 fecha, hora, id_tweet, nombre_usuario, tweet = linea["fecha"], linea["hora"], linea["id"], linea["username"], linea["text"]
                 lista_frases = [self.__limpiar(frase) for frase in tweet.split(".")]
@@ -39,14 +38,12 @@ class Indexador():
                 pares_id_tweet = pares_id_tweet + (id_tweet, tweet)
                 pares_fecha_id = pares_fecha_id + (datetime.datetime.strptime(f"{fecha} {hora}", "%d/%m/%Y %H:%M"), id_tweet)
 
-            self.__indice_frase_id = self.__ordenar_claves(self.__generar(pares_frase_id, indice_frase_id))
-            self.__indice_palabra_id = self.__ordenar_claves(self.__generar(pares_palabra_id, indice_palabra_id))
-            self.__indice_id_usuario = self.__ordenar_claves(self.__generar(pares_id_usuario, indice_id_usuario))
-            self.__indice_id_tweet = self.__ordenar_claves(self.__generar(pares_id_tweet, indice_id_tweet))
-            self.__indice_fecha_id = self.__ordenar_claves(self.__generar(pares_fecha_id, indice_fecha_id))
-            #ordenar valores
+            self.__indice_frase_id = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_frase_id, indice_frase_id)))
+            self.__indice_palabra_id = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_palabra_id, indice_palabra_id)))
+            self.__indice_id_usuario = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_id_usuario, indice_id_usuario)))
+            self.__indice_id_tweet = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_id_tweet, indice_id_tweet)))
+            self.__indice_fecha_id = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_fecha_id, indice_fecha_id)))
 
-            
     def __generar(self, lista_pares : list, dict_indice : dict):
         for par in lista_pares:
             #devuelve el valor de esa key, sino tiene valor, le crea
@@ -54,17 +51,15 @@ class Indexador():
             posting.add(par[1])
         return dict_indice
 
-    # def __ordenar_valores(diccionario : dict):
-    #     otro_diccionario = {}
-    #     for clave, valor in diccionario.items():
-    #         otro_diccionario.setdefault(clave)
-    #         ordenado = tuple(sorted(list(valor)))
-    #         otro_diccionario[clave] = ordenado
-    #     return otro_diccionario
-    
+    def __ordenar_valores(diccionario : dict):
+        otro_diccionario = {}
+        for clave, valor in diccionario.items():
+            otro_diccionario[clave] = tuple(sorted(list(valor)))
+        return otro_diccionario
+
     def __ordenar_claves(diccionario : dict):
         return dict(sorted(diccionario.items()))
-    
+
     def __limpiar(self, frase : str):
         frase = self.__quitar_hashtag(frase)
         frase = self.__sacar_puntuaciones(frase)
