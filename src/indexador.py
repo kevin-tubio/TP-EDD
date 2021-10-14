697d1ecc5ed697fefbd047d77b77d44faf6a469e
NicolasBenjaminCaceresMontenegro <88669210+NicolasBenjaminCaceresMontenegro@users.noreply.github.com>
2021-10-14 18:52:03 -0300

    indice fecha, posting
    agregado metodo generar que genera el posting
    agregado los indices
    ordenamiento de indices por clave
    falta ordenar los valores tengo un ordenador de valores, pero por ahi se puede mejorar

--- src/indexador.py
+++ src/indexador.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 import os
@@ -17,21 +18,18 @@ class Indexador():
         print(nombre_archivo)
         with open(nombre_archivo, "r", encoding="utf-8") as lector_csv:
             lector = csv.DictReader(lector_csv, delimiter=",")
-            self.__indice_id_tweet = {}
-            self.__indice_palabra_id = {}
-            self.__indice_id_usuario = {}
-            self.__indice_fecha_id = {}
-            self.__indice_frase_id = {}
+            indice_id_tweet = {}
+            indice_palabra_id = {}
+            indice_id_usuario = {}
+            indice_fecha_id = {}
+            indice_frase_id = {}
             pares_id_tweet = []
             pares_palabra_id = []
             pares_id_usuario = []
             pares_frase_id = []
             pares_fecha_id = []
+            
             for linea in lector:
-                # se podria hacer split(".") si tomamos como . el separador de frase, y despues split() con esa frase para las palabras?
-                #[palabra for palabras in lista_frases for palabra in palabras.split()]
-                #al usar esta lista hay que ver el tema del lematizado
-                #falta postin y faltan pares
                 fecha, hora, id_tweet, nombre_usuario, tweet = linea["fecha"], linea["hora"], linea["id"], linea["username"], linea["text"]
                 lista_frases = [self.__limpiar(frase) for frase in tweet.split(".")]
                 lista_palabras = [self.__lematizar(palabra) for palabras in lista_frases for palabra in palabras.split() if len(palabra) > 3 and not palabra in self.__stop_words]
@@ -39,9 +37,34 @@ class Indexador():
                 pares_palabra_id = pares_palabra_id + [(palabra, id_tweet) for palabra in lista_palabras]
                 pares_id_usuario = pares_id_usuario + (id_tweet, nombre_usuario)
                 pares_id_tweet = pares_id_tweet + (id_tweet, tweet)
-                
-                #self.__indice_fecha_id 
+                pares_fecha_id = pares_fecha_id + (datetime.datetime.strptime(f"{fecha} {hora}", "%d/%m/%Y %H:%M"), id_tweet)
 
+            self.__indice_frase_id = self.__ordenar_claves(self.__generar(pares_frase_id, indice_frase_id))
+            self.__indice_palabra_id = self.__ordenar_claves(self.__generar(pares_palabra_id, indice_palabra_id))
+            self.__indice_id_usuario = self.__ordenar_claves(self.__generar(pares_id_usuario, indice_id_usuario))
+            self.__indice_id_tweet = self.__ordenar_claves(self.__generar(pares_id_tweet, indice_id_tweet))
+            self.__indice_fecha_id = self.__ordenar_claves(self.__generar(pares_fecha_id, indice_fecha_id))
+            #ordenar valores
+
+            
+    def __generar(self, lista_pares : list, dict_indice : dict):
+        for par in lista_pares:
+            #devuelve el valor de esa key, sino tiene valor, le crea
+            posting = dict_indice.setdefault(par[0],set()) 
+            posting.add(par[1])
+        return dict_indice
+
+    # def __ordenar_valores(diccionario : dict):
+    #     otro_diccionario = {}
+    #     for clave, valor in diccionario.items():
+    #         otro_diccionario.setdefault(clave)
+    #         ordenado = tuple(sorted(list(valor)))
+    #         otro_diccionario[clave] = ordenado
+    #     return otro_diccionario
+    
+    def __ordenar_claves(diccionario : dict):
+        return dict(sorted(diccionario.items()))
+    
     def __limpiar(self, frase : str):
         frase = self.__quitar_hashtag(frase)
         frase = self.__sacar_puntuaciones(frase)
