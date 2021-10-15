581f8ec4308414b2389fb5135cb3cb4c12f97303
Kevin Tubio <kevin.tubio@hotmail.com>
2021-10-14 21:09:59 -0300

    Ahora indice de palabras no contiene emojis y demas

--- src/indexador.py
+++ src/indexador.py
@@ -11,7 +11,6 @@ class Indexador():
     def __init__(self) -> None:
         self.__stop_words = frozenset(stopwords.words('spanish'))
         self.__spanish_stemmer = SnowballStemmer('spanish', ignore_stopwords=False)
-        self.__puntuaciones = self.__obtener_puntuaciones()
 
     def armar_indices(self):
         nombre_archivo = os.path.abspath("fetched_tweets.csv")
@@ -31,7 +30,8 @@ class Indexador():
             for linea in lector:
                 fecha, hora, id_tweet, nombre_usuario, tweet = linea["fecha"], linea["hora"], linea["id"], linea["username"], linea["text"]
                 lista_frases = [self.__limpiar(frase) for frase in tweet.split(".")]
-                lista_palabras = [self.__lematizar(palabra) for palabras in lista_frases for palabra in palabras.split() if len(palabra) > 3 and not palabra in self.__stop_words]
+                lista_palabras = [self.__lematizar(palabra) for palabra in re.split("\W", str(lista_frases)) if self.__es_palabra_valida(palabra)]
+
                 pares_frase_id += [(frase, id_tweet) for frase in lista_frases]
                 pares_palabra_id += [(palabra, id_tweet) for palabra in lista_palabras]
                 pares_id_usuario += (id_tweet, nombre_usuario)
@@ -43,6 +43,9 @@ class Indexador():
             self.__indice_id_usuario = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_id_usuario, indice_id_usuario)))
             self.__indice_id_tweet = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_id_tweet, indice_id_tweet)))
             self.__indice_fecha_id = self.__ordenar_valores(self.__ordenar_claves(self.__generar(pares_fecha_id, indice_fecha_id)))
+
+    def __es_palabra_valida(self, palabra) -> bool:
+        return len(palabra) > 1 and palabra not in self.__stop_words
 
     def __generar(self, lista_pares : list, dict_indice : dict):
         for par in lista_pares:
@@ -65,21 +68,9 @@ class Indexador():
         frase = self.__sacar_puntuaciones(frase)
         return frase
 
-    def __quitar_hashtag(self, palabra : str):
-        return palabra.replace("#", "")
-
-    def __sacar_puntuaciones(self, palabra : str):
-        return palabra.strip(self.__puntuaciones)
-
     def __lematizar(self, palabra):
         palabra = self.__spanish_stemmer.stem(palabra)
         return palabra
-
-    def __obtener_puntuaciones(self):
-        puntuaciones = string.punctuation
-        for valor in range(128, 256):
-            puntuaciones += chr(valor)
-        return puntuaciones
 
     def print_indices(self):
         print(self.__indice_fecha_id)
