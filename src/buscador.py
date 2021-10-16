5d32c5ab5eab3492d9e5a51c0a3d90498c15bcf2
NicolasBenjaminCaceresMontenegro <88669210+NicolasBenjaminCaceresMontenegro@users.noreply.github.com>
2021-10-16 13:02:39 -0300

    minor fix
    ahora buscar palabra necesita una palabra lematizada (minimo de 2 caracteres, EJ: año -> añ)

--- src/buscador.py
+++ src/buscador.py
@@ -2,17 +2,6 @@ import datetime
 import re
 
 class Buscador:
-#BSBI
-#segmentar antes de indexar
-#generar indice segmentado
-#almacenar en disco
-#intercalar resultados para indice final
-#SPIMI
-#usa terminos en vez de pares?
-#guarda el dic del bloque en el disco
-#empieza un diccionario con cada nuevo bloque
-#MapReduce
-#dividir trabajo
 
     def __init__(self):
         self.__formato_hora = re.compile("^((0[0-9]|1[0-9]|2[0-3]):(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]))$")
@@ -23,24 +12,27 @@ class Buscador:
         dt = datetime.datetime.strptime(f"{self.__validar_fecha(fecha_1).group(1)} {self.__validar_hora(hora_1).group(1)}", "%d/%m/%Y %H:%M")
         n = 0
         lista_id = []
+        #opcional falta que aumente n por cada adicion pero no se como
+        #lista_id = [id for date, id in indice if dt <= date and n < cantidad]
         if fecha_2 == "" or hora_2 == "":
             for date, id in indice:
-                if date >= dt and n < cantidad:
+                if date <= dt and n < cantidad:
                     lista_id.append(id)
                     n += 1
-        else:         
+        else:
             dt2 = datetime.datetime.strptime(f"{self.__validar_fecha(fecha_2).group(1)} {self.__validar_hora(hora_2).group(1)}", "%d/%m/%Y %H:%M")
+            #opcional mismo problema que antes
+            #lista_id = [id for date, id in indice if dt <= date <= dt2 and n < cantidad]
             for date, id in indice:
-                if date >= dt and date <= dt2 and n < cantidad:
+                if dt <= date <= dt2 and n < cantidad:
                     lista_id.append(id)
                     n += 1
         return set(lista_id)
-
-    def buscar_palabra(self, palabra : str, indice : dict, cantidad):
+#se podria usar tambien en estos dos si encuentro la forma de contar mientras adiciona
+    def buscar_palabra(self, palabra_lematizada : str, indice : dict, cantidad):
         lista_palabra_id = []
-        if self.__validar_string(palabra) and self.__validar_cantidad(cantidad):
+        if self.__validar_string(palabra_lematizada) and self.__validar_cantidad(cantidad):
             n = 0
-            palabra_lematizada = ""#lematizar
             for palabra, id in indice:
                 if palabra == palabra_lematizada and n < cantidad:
                     n += 1
@@ -60,7 +52,7 @@ class Buscador:
         return cantidad > 0
 
     def __validar_string(self, string):
-        return type(string) == str and len(string) > 2
+        return type(string) == str and len(string) >= 2
 
     def __validar_fecha(self, fecha : str):
         return self.__formato_fecha.match(fecha)
